d8a42e42c47e950961fd8fe41d8b9114df0b6435
Daian <daian.aiziatov@gmail.com>
2019-03-26 12:31:09 -0400

    Project setup

---
+++ TradeRevChallenge/Controllers/PhotosViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TradeRevChallenge
+//
+//  Created by Daian Aiziatov on 26/03/2019.
+//  Copyright © 2019 Daian Aiziatov. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
